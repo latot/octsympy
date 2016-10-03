0090a325fb5a3d000ec1f5897e59d1dc6a579e46
Abhinav Tripathi <genuinelucifer@gmail.com>
2016-10-04 00:00:53 +0530

    Add cell_array_to_sym as private static method.

--- inst/@sym/sym.m
+++ inst/@sym/sym.m
@@ -195,7 +195,27 @@ classdef sym < handle
         s = '';
         flag = 0;
       end
+    end
+  end
 
+  methods(Static, Access = private)
+    function s = cell_array_to_sym (L)
+    %private helper for sym ctor
+    %   convert a cell array to syms, recursively when nests cells found
+      assert(iscell(L))
+
+      s = cell(size(L));
+
+      for i = 1:numel(L)
+        %s{i} = sym(L{i});
+        % not strictly necessary if sym calls this but maybe neater this way:
+        item = L{i};
+        if iscell(item)
+          s{i} = cell_array_to_sym(item);
+        else
+          s{i} = sym(item);
+        end
+      end
     end
   end
 
@@ -411,7 +431,7 @@ classdef sym < handle
   methods(Static)
       function symout = symarray(argin)
          if (iscell (argin))
-          symout = cell_array_to_sym(argin);
+          symout = sym.cell_array_to_sym(argin);
          else
           symout = sym(argin);
          end
